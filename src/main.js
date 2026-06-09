aa2a3aa369cc6ab440e8d5c61383cab92ccdea7c
Valeria Pikul <valeriapikul@yandex.ru>
2026-06-09 16:30:06 +0300

    correct main file

--- src/main.js
+++ src/main.js
@@ -39,9 +39,6 @@ const init = () => {
             })
             .then((parsed) => {
 
-                console.log('parsed:', parsed);
-                console.log('posts count:', parsed.posts.length);
-
                 const feedId = Date.now();
 
                 state.feeds.push({
@@ -66,9 +63,6 @@ const init = () => {
 
                 input.value = '';
                 input.focus();
-
-                console.log('feed:', feed);
-                console.log('posts:', posts);
 
             })
             .catch((e) => {
